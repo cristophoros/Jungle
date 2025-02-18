fa16b17cbf448a3df92d17e2fdfd359ba3d6136c
cristophoros <gocherochristopher@gmail.com>
2025-02-18 16:16:56 +0200

    Create program.cs
    This is a stupid code dont pay attention im still new to programming and github.

---
+++ program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nextlevel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 20;
+            string result = (time < 18) ? "Good day" : "good evening";            
+                Console.WriteLine(result); 
+            
+
+        }
+    }
+}
